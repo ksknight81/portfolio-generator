2927f7524375ea7c64f95af13148d36b37f5f5e2
kevin knight <ksknight81@gmail.com>
2022-06-27 12:45:39 -0400

    added code template

--- app.js
+++ app.js
@@ -3,41 +3,16 @@ const generatePage = require('./src/page-template.js');
 
 const profileDataArgs = process.argv.slice(2);
 
+console.log(profileDataArgs);
+
 const [name, github] = profileDataArgs;
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+console.log(name, github);
 
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+const pageHTML = generatePage(name, github);
 
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-
-// displays the name
-// const generatePage = (userName, githubName) => {
-//   return `
-//     Name: ${userName}
-//     Github: ${githubName}
-//   `;
-// };
-
-
-
-// console.log(name, github);
-// console.log(generatePage(name, github));
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw err;
 
-  console.log('Portfolio complete!  Checkout index.html to see the output!');
+  console.log('Portfolio complete! Check out index.html to see the output!');
 });
