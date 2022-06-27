0a103a9c397352a90cda85e98c1b358383560fc2
kevin knight <ksknight81@gmail.com>
2022-06-27 12:27:45 -0400

    added html creation to file

--- app.js
+++ app.js
@@ -1,62 +1,43 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// notice the lack fo parentheses around the 'profileDataArr' parameter?
-const printProfileData = profileDataArr => {
-    // This...
+const profileDataArgs = process.argv.slice(2);
 
-    for (let i = 0; i < profileDataArr.length; i+= 1){
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('=============');
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
-printProfileData(profileDataArgs);
+//   console.log('================');
 
-// var is function-scoped, so redeclaring it in a block will cause its value outside the block to change as well:
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-var one = 'one: declared outside the block';
+// printProfileData(profileDataArgs);
 
-if (true === true) {
-  var one = 'one: declared inside the block'; // notice: we redeclare 'one' here
-  console.log(one); // prints 'one: declared inside the block'
-}
 
-console.log(one); // also prints 'one: declared inside the block', because the variable was redeclared in the 'if' block. The outer 'var' variable was therefore destroyed and replaced by inner var variable.
+// displays the name
+// const generatePage = (userName, githubName) => {
+//   return `
+//     Name: ${userName}
+//     Github: ${githubName}
+//   `;
+// };
 
-// 'let' is block-scoped, so redeclaring a 'let' variable inside of a block creates a different 'let' variable with the same name whose scope is inside the block:
 
-let two = 'two: declared outside the block';
 
-if (true === true) {
-  let two = 'two: declared inside the block';
-  console.log(two); // prints 'two: declared inside the block'
-}
+// console.log(name, github);
+// console.log(generatePage(name, github));
 
-console.log(two); // prints 'two: declared outside the block', because two declared inside the block is a separate variable. The 'let' variables are unrelated and therefore are unaffected by each other.
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-// var is function-scoped, so changing its value in a block causes its value in the outer environment to change as well:
-
-var three = 'three: declared outside the block';
-
-if (true === true) {
-  three = 'three: changed inside the block'; // notice: we don't redeclare
-  console.log(three); // prints 'three: changed inside the block'
-}
-
-console.log(three); // also prints 'three: changed inside the block', because the variable has function scope. This means that the value change in the block is reflected throughout the function, i.e., outside the block.
-
-// let is block-scoped, so changing its value in a block does change its value outside the block _if_ the variable is not redeclared in the block:
-
-let four = 'four: outside the block';
-
-if (true === true) {
-  four = 'four: inside the block'; // notice: we don't redeclare the variable
-  console.log(four); // prints 'four: inside the block'
-}
-
-console.log(four); // prints 'four: inside the block', because we didn't redeclare the variable inside the block. That meant we referenced the variable outside the block, and we therefore changed it.
+  console.log('Portfolio complete!  Checkout index.html to see the output!');
+});
